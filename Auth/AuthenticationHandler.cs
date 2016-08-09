af9824e4f8a5bd3affdbbfc251836e7861f01c8d
douglas <douglas.picolotto@gmail.com>
2016-08-09 19:43:35 -0300

    Alteração do método de validação do usuário

--- Auth/AuthenticationHandler.cs
+++ Auth/AuthenticationHandler.cs
@@ -4,39 +4,19 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace AspNetCoreDocker.Auth
 {
-    public class AuthenticationHandler : IJwtBearerEvents
+    public class AuthenticationHandler : JwtBearerEvents
     {
-        private EFContext _context;
-
-        public AuthenticationHandler(EFContext context)
-        {
-            _context = context;
-        }
-
-        public Task AuthenticationFailed(AuthenticationFailedContext context)
-        {
-            return Task.FromResult(0);
-        }
-
-        public Task Challenge(JwtBearerChallengeContext context)
-        {
-            return Task.FromResult(0);
-        }
-
-        public Task MessageReceived(MessageReceivedContext context)
-        {
-            return Task.FromResult(0);
-        }
-
-        public Task TokenValidated(TokenValidatedContext context)
+        public override Task TokenValidated(TokenValidatedContext context)
         {
             var userName = context.Ticket.Principal.Identity.Name;
 
-            if(!_context.Users.Any(u=> u.UserName == userName))
+            var dbContext = context.HttpContext.RequestServices.GetService(typeof(EFContext)) as EFContext;
+            
+            if(!dbContext.Users.Any(u=> u.UserName == userName))
             {
                 context.Response.StatusCode = 401;    
                 context.SkipToNextMiddleware();   
-            }
+            }            
 
             return Task.FromResult(0);
         }
